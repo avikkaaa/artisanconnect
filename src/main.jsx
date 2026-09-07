082444d0c8c7728e606a7b55b3dfd2d389e91387
avikkaaa <avikaar1176@gmail.com>
2026-09-08 00:30:21 +0530

    Use polished simple aesthetic ArtisanConnect frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppFixed';
+import App from './AppFinal';
 import LiveAI from './LiveAI';
 import './index.css';
 
