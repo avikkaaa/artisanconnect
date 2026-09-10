786c0c845044ca186498990e8f11882ea7a859b6
avikkaaa <avikaar1176@gmail.com>
2026-09-10 16:09:00 +0530

    Use new ecommerce-style ArtisanConnect frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppFinal';
+import App from './AppMarketplace';
 import LiveAI from './LiveAI';
 import './index.css';
 
