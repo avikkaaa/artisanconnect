fc74155ab1e9ccc47c282f90cb1fcdf638f59c6c
avikkaaa <avikaar1176@gmail.com>
2026-09-11 16:26:35 +0530

    Remove broken unused LiveAI entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import LiveAI from './LiveAI';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <LiveAI />
   </React.StrictMode>
 );
